1640fa0f90d7d486f4894a4cf8b28505c315c2e3
Cheyanne Cabang <cheyannephongsavath@gmail.com>
2020-12-11 22:40:52 -0500

    added p3 visualizations

--- P3.py
+++ P3.py
@@ -1,10 +1,12 @@
 # Final Project Question 3
 # Cheyanne Cabang & Ricardo Sauerbrey
 
-import pandas
+import pandas as pd
 import csv
+import matplotlib.pyplot as plt
+import numpy as np
 
-file = pandas.read_csv("1976-2016-president.csv")  # Import the file to Python
+file = pd.read_csv("1976-2016-president.csv")  # Import the file to Python
 
 
 def rep_states2012():
@@ -22,11 +24,11 @@ def rep_states2012():
         for row in myfile_reader:
             if row[0] == year and row[8] == "republican" and float(row[14][:len(row[14]) - 1]) > 62.29:  # Values to Search for
                 print(row[1], "with a supporting rate of", row[14])  # Desired Output Layout
-                rep_states_2012.append(row[14])  # Appending each output to the list
+                rep_states_2012.append((row[1], float(row[14].strip("%"))))  # Appending each output to the list
 
         return rep_states_2012
 
-rep_states2012()
+
 print("\n")  # For Organization
 
 
@@ -45,9 +47,10 @@ def dem_states2012():
         for row in myfile_reader:
             if row[0] == year and row[8] == "democrat" and float(row[14][:len(row[14]) - 1]) > 61:  # Values to Search for
                 print(row[1], "with a supporting rate of", row[14])  # Desired Output Layout
-                dem_states_2012.append(row[14])  # Appending each output to the list
+                dem_states_2012.append((row[1], float(row[14].strip("%"))))  # Appending each output to the list
 
         return dem_states_2012
+
 
 print("\n")  # For Organization
 
@@ -66,7 +69,7 @@ def rep_states2016():
         for row in myfile_reader:
             if row[0] == year and row[8] == "republican" and float(row[14][:len(row[14]) - 1]) > 62.50:  # Values to Search for
                 print(row[1], "with a supporting rate of", row[14])  # Desired Output Layout
-                rep_states_2016.append(row[14])  # Appending each output to the list
+                rep_states_2016.append((row[1], float(row[14].strip("%"))))  # Appending each output to the list
 
         return rep_states_2016
 
@@ -89,9 +92,106 @@ def dem_states2016():
         for row in myfile_reader:
             if row[0] == year and row[8] == "democrat" and float(row[14][:len(row[14]) - 1]) > 59.00:  # Values to Search for
                 print(row[1], "with a supporting rate of", row[14])  # Desired Output Layout
-                dem_states_2016.append(row[14])  # Appending each output to the list
+                dem_states_2016.append((row[1], float(row[14].strip("%"))))  # Appending each output to the list
 
         return dem_states_2016
 
 
 print("\n")  # For Organization
+
+
+def dem_states_2012_plot():
+    """
+    Plots top 5 democrat states in 2012
+    """
+
+    data = dem_states2012()
+    state = []
+    supporting_rates = []
+
+    for i in data:
+        state.append(i[0])
+        supporting_rates.append(i[1])
+
+    dataframe = pd.DataFrame({"State": state, "Supporting Rates": supporting_rates}, index=state)
+    dataframe = dataframe.sort_values("Supporting Rates")
+    dataframe.plot(kind="barh", color="blue")
+    plt.gcf().subplots_adjust(bottom=0.15, left=0.3)
+    plt.title("Top 5 Democrat-supporting States in 2012")
+    plt.xlabel("Supporting Rates (%)")
+    plt.ylabel("State")
+    plt.savefig("demstates2012.png")
+    return plt.show()
+
+
+def rep_states_2012_plot():
+    """
+    Plots top 5 republican states in 2012
+    """
+
+    data = rep_states2012()
+    state = []
+    supporting_rates = []
+
+    for i in data:
+        state.append(i[0])
+        supporting_rates.append(i[1])
+
+    dataframe = pd.DataFrame({"State": state, "Supporting Rates": supporting_rates}, index=state)
+    dataframe = dataframe.sort_values("Supporting Rates")
+    dataframe.plot(kind="barh", color="r")
+    plt.gcf().subplots_adjust(bottom=0.15, left=0.3)
+    plt.title("Top 5 Republican-supporting States in 2012")
+    plt.xlabel("Supporting Rates (%)")
+    plt.ylabel("State")
+    plt.savefig("repstates2012.png")
+    return plt.show()
+
+
+def dem_states_2016_plot():
+    """
+    Plots top 5 democrat states in 2016
+    """
+
+    data = dem_states2016()
+    state = []
+    supporting_rates = []
+
+    for i in data:
+        state.append(i[0])
+        supporting_rates.append(i[1])
+
+    dataframe = pd.DataFrame({"State": state, "Supporting Rates": supporting_rates}, index=state)
+    dataframe = dataframe.sort_values("Supporting Rates")
+    dataframe.plot(kind="barh", color="blue")
+    plt.gcf().subplots_adjust(bottom=0.15, left=0.3)
+    plt.title("Top 5 Democrat-supporting States in 2016")
+    plt.xlabel("Supporting Rates (%)")
+    plt.ylabel("State")
+    plt.savefig("demstates2016.png")
+    return plt.show()
+
+
+def rep_states_2016_plot():
+    """
+    Plots top 5 republican states in 2016
+    """
+
+    data = rep_states2016()
+    state = []
+    supporting_rates = []
+
+    for i in data:
+        state.append(i[0])
+        supporting_rates.append(i[1])
+
+    dataframe = pd.DataFrame({"State": state, "Supporting Rates": supporting_rates}, index=state)
+    dataframe = dataframe.sort_values("Supporting Rates")
+    dataframe.plot(kind="barh", color="r")
+    plt.gcf().subplots_adjust(bottom=0.15, left=0.3)
+    plt.title("Top 5 Republican-supporting States in 2016")
+    plt.xlabel("Supporting Rates (%)")
+    plt.ylabel("State")
+    plt.savefig("repstates2016.png")
+    return plt.show()
+
